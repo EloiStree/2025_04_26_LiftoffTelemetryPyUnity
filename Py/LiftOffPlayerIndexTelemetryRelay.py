57f2d61acf933097780f2203e0dc31a1920584e8
Eloi Stree <eloistree@gmail.com>
2025-05-26 23:32:47 +0200

    save

--- Py/LiftOffPlayerIndexTelemetryRelay.py
+++ Py/LiftOffPlayerIndexTelemetryRelay.py
@@ -65,7 +65,7 @@ def listen_to_udp(port):
                 bytes_telemetry = bytearray(data)
                 # prepend player index as little endian int
                 int_lenght = len(bytes_telemetry)
-                print(f"Received telemetry data of length {int_lenght} from {client_address}")
+                #print(f"Received telemetry data of length {int_lenght} from {client_address}")
                 bytes_telemetry = struct.pack('<i', player_index) + bytes_telemetry
 
                 for target_ip, redirection_port in targets_list_string_ipv4_port:
